dc4242c06bd38b4c4807dd78100e0254c009c914
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 08:32:54 -0400

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,38 @@
+let XPos = 25;
+function setup() {
+  createCanvas(500, 500);
+background(0);
+}
+
+   
+  let xPos = random(0,500)
+    let yPos = random(0,500)
+    ellipse(xPos, yPos, 25, 25);
+
+}
+
+function draw(){
+  fill(0, 255, 0);
+  ellipse(xPos, 250, 50, 50);
+  xPos += 3;
+  
+  let r = random(0,255);
+  let g = random(0,255);
+  let b = random(0,255);
+  fill(r,g,b)
+  
+  let xPos = random(0,500)
+  let yPos = random(0,500)
+ 
+  
+ 
+  
+}
+function mouseClicked(){
+  nos
+  let r = random(0,255);
+  let g = random(0,255);
+  let b = random(0,255);
+  fill(r,g,b)
+  ellipse(mouseX, mouseY, 25, 25);
+  }
